78d4f92c41185638d101e24f78a41d6a4209a7c1
agecage <35671542+agecage@users.noreply.github.com>
2018-01-25 20:16:57 -0500

    Fixes #2
    Fixes #2

--- PlayerActivity.py
+++ PlayerActivity.py
@@ -20,11 +20,14 @@ filename_OutfitName = path_UserInformation + "OutfitName.txt"
 
 filename_OutfitID = path_UserInformation + "OutfitID.txt"
 
+# Storage location for final data
+filename_OutfitData = path_UserInformation + "OutfitData.csv"
+
 # %% Load the user specific data
 
 with open(filename_UserID) as f:
     UserName = f.read()
-    
+
 with open(filename_OutfitName) as f:
     OutfitName = f.read()
 
@@ -47,12 +50,12 @@ with open(filename_OutfitName) as f:
 #
 #with open(filename_OutfitID, 'w') as f:
 #    f.write(OutfitID)
-#    
+#
 # %% Load the outfit ID if this doesn't work run commented previous cell
 
 with open(filename_OutfitID) as f:
     OutfitID =f.read()
-    
+
 # %% Get a list of the outfit members
 
 request = requests.get("http://census.daybreakgames.com/s:" + UserName +
@@ -85,21 +88,26 @@ df_OutfitMembers['Name']=''
 
 for index in df_OutfitMembers['character_id'].index:
     CharacterID = df_OutfitMembers.loc[index,'character_id']
-    
+
 
     request = requests.get("http://census.daybreakgames.com/s:" + UserName +
                        "/get/ps2:v2/character/?character_id=" + CharacterID)
     try:
-        LastLoginDate = json.loads(request.text)['character_list'][0]['times']['last_login_date']    
+        LastLoginDate = json.loads(request.text)['character_list'][0]['times']['last_login_date']
         CurrentUserName = json.loads(request.text)['character_list'][0]['name']['first']
         df_OutfitMembers.loc[index, 'LastLoginDate'] = LastLoginDate
         df_OutfitMembers.loc[index, 'Name'] = CurrentUserName
     except KeyError:
         print(request.text)
         break
-    
-    
-    
+
+# %% Save dataframe as a .csv
+
+df_OutfitMembers = df_OutfitMembers.loc[:, ['Name', 'rank', 'LastLoginDate', 'member_since_date', 'rank_ordinal', 'character_id']]
+
+### TODO: sort dataframe based on last login date
+
+df_OutfitMembers.to_csv(filename_OutfitData)
 
 ## %% Making a concatenated string containing all of the user IDs separated by commas for use in the URL request
 #
@@ -109,7 +117,7 @@ for index in df_OutfitMembers['character_id'].index:
 #str_CharacterIDs = str_CharacterIDs[:-1]
 #
 ## %% Requesting all of the user IDs in a single request
-## This failed because the URL was too long.    
+## This failed because the URL was too long.
 #
 ##for CharacterID in df_OutfitMembers['character_id']:
 #    # print(CharacterID)
@@ -120,6 +128,6 @@ for index in df_OutfitMembers['character_id'].index:
 ##    PlayerName = TempJSON['name']['first']
 ##    PlayerName = CharacterID
 ##    LastSaveDate = TempJSON['times']['last_save_date']
-#    
+#
 ##    print(PlayerName, LastSaveDate)
-##    
+##
